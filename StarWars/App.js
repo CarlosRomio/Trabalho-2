e54148a9ded263a21e78c80ec26eaa2dc03b19fa
Carlos Bezerra <carlosromiobezerra@gmail.com>
2024-09-12 11:38:56 -0300

    .

--- StarWars/App.js
+++ StarWars/App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import * as React from 'react';
+//import { NavigationContainer } from '@react-navigation/native';
+//import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+//import CharactersScreen from '/screens/CharactersScreen.js';
+//import CharacterDetailsScreen from './screens/CharacterDetailsScreen';
+//import StarshipsScreen from './screens/StarshipsScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+//const Stack = createStackNavigator();
+
+//export default function App() {
+    //return (
+        //<NavigationContainer>
+            //<Stack.Navigator initialRouteName="Characters">
+                //<Stack.Screen name="Characters" component={CharactersScreen} />
+                //<Stack.Screen name="CharacterDetails" component={CharacterDetailsScreen} />
+               //<Stack.Screen name="Starships" component={StarshipsScreen} />
+                //{/* Adicionar mais telas quando criadas */}
+            //</Stack.Navigator>
+        //</NavigationContainer>
+    //);
+//}
